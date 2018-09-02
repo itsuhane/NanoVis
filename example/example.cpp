6f58c72c9fb5322e3f706d8e25bd5685b96d479e
itsuhane <itsuhane@gmail.com>
2018-09-02 17:48:02 +0800

    well, a better example.

--- example/example.cpp
+++ example/example.cpp
@@ -10,9 +10,21 @@ class Example : public nanovis::NanoVis {
     Example() :
         NanoVis("NanoVis Example", 500, 500) {
         value = 0.5; // don't start with 0 or 1 :P
-        points.resize(100);
-        for (auto& p : points) {
-            p.setRandom();
+        points.resize(1000);
+        {
+            double h = 0.01;
+            double a = 10.0;
+            double b = 28.0;
+            double c = 8.0 / 3.0;
+            Eigen::Vector3f x0 = {0.1, 0.0, 0.0};
+            Eigen::Vector3f x1;
+            for (size_t i = 0; i < points.size(); ++i) {
+                x1.x() = x0.x() + h * a * (x0.y() - x0.x());
+                x1.y() = x0.y() + h * (x0.x() * (b - x0.z()) - x0.y());
+                x1.z() = x0.z() + h * (x0.x() * x0.y() - c * x0.z());
+                points[i] = x1 * 0.1;
+                x0 = x1;
+            }
         }
 
         add_graph("Panel B", "value", value, 1.0, 0.0);
@@ -22,15 +34,25 @@ class Example : public nanovis::NanoVis {
         add_button("Panel A", "Button A", [this]() {
             value = 3.772 * value * (1 - value); // generate some chaos by logistic map
             notify(value);                       // this refreshes all widgets who are observing value
-            for (auto& p : points) {
-                p.setRandom();
-            }
-            notify(points);
         });
 
         add_repeat("Panel A", "Button B", [this]() {
             value = 3.772 * value * (1 - value);
             notify(value);
+            for (size_t i = 1; i < points.size(); ++i) {
+                points[i - 1] = points[i];
+            }
+            double h = 0.01;
+            double a = 10.0;
+            double b = 28.0;
+            double c = 8.0 / 3.0;
+            const Eigen::Vector3f x0 = points.back() * 10;
+            Eigen::Vector3f x1;
+            x1.x() = x0.x() + h * a * (x0.y() - x0.x());
+            x1.y() = x0.y() + h * (x0.x() * (b - x0.z()) - x0.y());
+            x1.z() = x0.z() + h * (x0.x() * x0.y() - c * x0.z());
+            points.back() = x1 * 0.1;
+            notify(points);
             return true; // returning false can force repeat to end
         });
 
